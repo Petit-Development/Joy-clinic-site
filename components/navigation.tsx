e9a61159f16acda960fa19074f360397018ae503
Joshua Petitma <joshua@petit.dev>
2021-11-03 14:26:34 -0400

    Create basic nav bar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 interface NavigationProps {}
 
 const Navigation: React.FC<NavigationProps> = ({}) => (
-  <nav className="fixed inset-x-0 bg-gray-100 opacity-75">
-    {buttons.map((but) => (
-      <NavigationButton name={but.name} dest={but.dest} />
-    ))}
+  <nav className="fixed inset-x-0 bg-gray-100 opacity-75 space-x-0">
+    <div className="flex justify-center lg:space-x-24 md:space-x-12 space-x-10 ">
+      {buttons.map((but) => (
+        <NavigationButton name={but.name} dest={but.dest} />
+      ))}
+    </div>
   </nav>
 );
 
@@ -13,8 +16,12 @@ interface NavigationButtonProps {
   dest: string;
 }
 
-const NavigationButton: React.FC<NavigationButtonProps> = ({}) => (
-  <div>Sup</div>
+const NavigationButton: React.FC<NavigationButtonProps> = ({ name, dest }) => (
+  <div className="flex items-center h-14 lg:h-20 hover:underline">
+    <Link href={dest}>
+      <a className="lg:text-2xl md:text-2xl">{name}</a>
+    </Link>
+  </div>
 );
 const buttons = [
   {
