906e088adcfa6cdd94b26724d449a02cd581a434
Joshua Petitma <joshua@petit.dev>
2021-11-04 15:01:56 -0400

    Add structure for prevention section

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,27 +4,22 @@ interface NavigationProps {}
 
 const Navigation: React.FC<NavigationProps> = ({}) => (
   <div className="flex content-center justify-center align-middle">
-    <nav className="fixed inset-x-0 opacity-75 space-x-0">
-      <div className="flex justify-center lg:space-x-24 md:space-x-12 space-x-10 ">
+    <nav className="fixed inset-x-0 z-10 opacity-90 space-x-0 bg-gray-50">
+      <div className="flex justify-center lg:space-x-24 md:space-x-12 space-x-10">
         {buttons.map((but, ind) => (
           <NavigationButton key={ind} name={but.name} dest={but.dest} />
         ))}
       </div>
     </nav>
-    {
-      //<NavigationButton name={''} dest={''} />
-    }
-    <div className="invisible py-24 lg:visible sm:invisible">
-      <div className="">
+    <div className="pb-20"></div>
+    <div className="hidden pt-24 md:visible lg:inline-block">
         <Image
-          className="rounded-2xl"
           src={"/header.png"}
           objectFit="contain"
           layout="fixed"
           width={1400}
           height={283}
         />
-      </div>
     </div>
   </div>
 );
@@ -37,7 +32,7 @@ interface NavigationButtonProps {
 const NavigationButton: React.FC<NavigationButtonProps> = ({ name, dest }) => (
   <div className="flex items-center h-14 lg:h-20 hover:underline">
     <Link href={dest}>
-      <a className="text-4xl lg:text-2xl md:text-2xl sm:text-xl">{name}</a>
+      <a className="text-center text-l lg:text-2xl md:text-2xl sm:text-xl">{name}</a>
     </Link>
   </div>
 );
